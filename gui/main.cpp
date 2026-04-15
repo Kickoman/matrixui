e7cb92463a660ac31a7d73156a7101eff44eafe6
kanstancin <kastus.novikau@gmail.com>
2026-04-15 21:17:45 +0300

    Fix buttons

--- gui/main.cpp
+++ gui/main.cpp
@@ -15,30 +15,6 @@ int main(int argc, char** argv) {
     MainWindow window;
     window.setController(&controller);
     window.handleNewTabRequested();
-
-
-    QSettings settings;
-    auto allKeys = settings.allKeys();
-    for (const auto& key : allKeys) {
-        qDebug() << key << " ";
-    }
-    // QSettings settings;
-    // Neural::Trainer recognizer;
-    // DigitsRecognizerController controller(&recognizer);
-    // controller.loadNetwork(settings.value("last_network_name", QString("network.wgt")).toString());
-    // controller.setTrainingDataset(settings.value(
-    //     "last_training_dataset_path",
-    //     QString("/home/kanstancin/Documents/projects/digits-generator/digit_images/")
-    // ).toString());
-    // controller.setTestingDataset(settings.value(
-    //     "last_testing_dataset_path",
-    //     QString("/home/kanstancin/Documents/projects/digits-generator/digit_images/")
-    // ).toString());
-
-    // recognizer.setOutputStream(stream.get());
-    // window.setLogger(stream.get());
-    // window.setController(&controller);
-
     window.show();
     return a.exec();
 }
--- gui/new_mode_widget.cpp
+++ gui/new_mode_widget.cpp
@@ -1,17 +1,31 @@
 #include "new_mode_widget.h"
 #include "mode_factory.h"
 
-#include <QPushButton>
-#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QToolButton>
+#include <qtoolbutton.h>
 
 NewModeWidget::NewModeWidget(QWidget* parent)
     : ModeWidget("New mode", parent)
 {
-    auto* digitsClassifierButton = new QPushButton("Digits classifier", this);
-    auto* digitsRecognitionPlaygroundButton = new QPushButton("Digits recognition playground", this);
-    auto* ganButton = new QPushButton("GAN generative network training", this);
+    auto* digitsClassifierButton = new QToolButton(this);
+    auto* digitsRecognitionPlaygroundButton = new QToolButton(this);
+    auto* ganButton = new QToolButton(this);
 
-    auto* layout = new QVBoxLayout(this);
+    digitsClassifierButton->setText("Digits\nclassifier");
+    digitsRecognitionPlaygroundButton->setText("Digits\nrecognition\nplayground");
+    ganButton->setText("GAN\ngenerative\nnetwork\ntraining");
+
+    const QSize buttonSize(300, 300);
+    const QFont buttonFont("sans", 24);
+    digitsClassifierButton->setFixedSize(buttonSize);
+    digitsClassifierButton->setFont(buttonFont);
+    digitsRecognitionPlaygroundButton->setFixedSize(buttonSize);
+    digitsRecognitionPlaygroundButton->setFont(buttonFont);
+    ganButton->setFixedSize(buttonSize);
+    ganButton->setFont(buttonFont);
+
+    auto* layout = new QHBoxLayout(this);
     layout->addWidget(digitsClassifierButton);
     layout->addWidget(digitsRecognitionPlaygroundButton);
     layout->addWidget(ganButton);
@@ -20,17 +34,17 @@ NewModeWidget::NewModeWidget(QWidget* parent)
 
     connect(
         digitsClassifierButton,
-        &QPushButton::clicked,
+        &QToolButton::clicked,
         [this]{ emit modeRequested(ModeType::DigitsClassifier); }
     );
     connect(
         digitsRecognitionPlaygroundButton,
-        &QPushButton::clicked,
+        &QToolButton::clicked,
         [this]{ emit modeRequested(ModeType::DigitsRecognizer); }
     );
     connect(
         ganButton,
-        &QPushButton::clicked,
+        &QToolButton::clicked,
         [this]{ emit modeRequested(ModeType::DigitsGenerator); }
     );
 }
